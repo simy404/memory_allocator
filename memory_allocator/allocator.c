7d45be6c0ad4df8abf3676bd603852b906140767
simy404 <asimetra404@gmail.com>
2025-04-24 20:07:12 +0200

    fix: update memory block linking in safe_malloc function

--- memory_allocator/allocator.c
+++ memory_allocator/allocator.c
@@ -6,7 +6,7 @@
 /*   By: hsamir <hsamir@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 15:41:39 by hsamir            #+#    #+#             */
-/*   Updated: 2025/04/24 19:36:15 by hsamir           ###   ########.fr       */
+/*   Updated: 2025/04/24 20:04:16 by hsamir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,17 @@ void	*safe_malloc(size_t size, t_mem_type mem_type)
 	t_memory_block	*mem_block;
 	t_memory_block	**head;
 
-	mem_block = (t_memory_block*)malloc(sizeof(t_memory_block) + size);
+	mem_block = malloc(sizeof(t_memory_block) + size);
 	if (mem_block == NULL)
 		safe_abort(STD_EXIT);
+	head = get_memory_head(mem_type);
 	*mem_block = (t_memory_block){
 		.magic = MAGIC | (size_t)mem_type,
-		.next = NULL,
-		.prev = NULL
+		.next = *head,
+		.prev = NULL,
 	};
-	head = get_memory_head(mem_type);
 	if (*head != NULL)
-	{
-		mem_block->next = *head;
 		(*head)->prev = mem_block;
-	}
 	*head = mem_block;
 	return (mem_block + 1);
 }
